65b41e7cff5d50810741d414ce7fe883330a7a33
vani@linux <winfkill@gmail.com>
2023-02-12 12:43:50 +0200

    Custom domain url

--- pateik_core/settings.py
+++ pateik_core/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.environ.get("DJANGO_DEBUG","") != "False"
 
 
-ALLOWED_HOSTS = ["127.0.0.1","commerce-pwjx.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1","commerce-pwjx.onrender.com","pate1k.com"]
 AUTH_USER_MODEL = "pateik_app.Customer"
 
 # Application definition
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/images")
 MEDIA_URL = "/media/"
 FIXTURE_DIRS = (os.path.join(BASE_DIR, "fixtures"),)
-ACTUAL_DOMAIN_URL = "http://127.0.0.1:8000/"
+ACTUAL_DOMAIN_URL = "https://pate1k.com/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
